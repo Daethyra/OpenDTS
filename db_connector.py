1c4f851b877be6113dbaaaa02dfddbb431ff3c41
Daemon <109057945+Daethyra@users.noreply.github.com>
2023-02-05 22:34:19 -0800

    Update db_connector.py

--- db_connector.py
+++ db_connector.py
@@ -1,5 +1,6 @@
 import logging
 import traceback
+import pandas as pd
 from functools import lru_cache
 
 @lru_cache(maxsize=2000)
@@ -18,11 +19,14 @@ def get_sentiment_results():
         cursor.execute(query)
         sentiment_results = cursor.fetchall()
 
+        # Convert the results to a Pandas DataFrame
+        df = pd.DataFrame(sentiment_results, columns=['username', 'text', 'sentiment'])
+
         # Close the database connection
         cursor.close()
         conn.close()
 
-        return sentiment_results
+        return df
     except Exception as e:
         # Log the error message and stack trace in case of any exceptions
         logging.error(f"An error occurred while fetching sentiment results: {e}")
@@ -45,11 +49,14 @@ def get_keywords():
         cursor.execute(query)
         keywords = cursor.fetchall()
 
+        # Convert the keywords to a list
+        keywords_list = [k[0] for k in keywords]
+
         # Close the database connection
         cursor.close()
         conn.close()
 
-        return keywords
+        return keywords_list
     except Exception as e:
         # Log the error message and stack trace in case of any exceptions
         logging.error(f"An error occurred while fetching keywords: {e}")
