75f74232f299df4a7efad62b889f386ec0a96281
Daemon <109057945+Daethyra@users.noreply.github.com>
2023-02-11 21:34:04 -0800

    Update db_connector.py

--- db_connector.py
+++ db_connector.py
@@ -1,12 +1,20 @@
 import logging
 import traceback
 import pandas as pd
-from functools import lru_cache
 import psycopg2
+import psycopg2.pool
 import os
 import dotenv
+import re
+import tempfile
+import shutil
+import zipfile
+from functools import lru_cache
+from datetime import datetime, timedelta
 
 dotenv.load_dotenv()
+
+ARCHIVE_FOLDER = 'archive'
 
 def connect_to_db():
     try:
@@ -22,62 +30,41 @@ def connect_to_db():
         )
         # Get a connection from the pool
         connection = connection_pool.getconn()
-        # Return the connection
-        return connection
+
+        # Set autocommit to True for executemany to work
+        connection.autocommit = True
+
+        # Check if the archive folder exists
+        if not os.path.exists(ARCHIVE_FOLDER):
+            os.makedirs(ARCHIVE_FOLDER)
+            first_run = True
+        else:
+            # Check the creation time of the archive folder
+            archive_creation_time = datetime.fromtimestamp(os.path.getctime(ARCHIVE_FOLDER))
+            threshold_time = datetime.now() - timedelta(days=28)
+            first_run = False
+
+            # If the archive folder was created more than 28 days ago, archive log files and any other files meeting the threshold
+            if archive_creation_time < threshold_time:
+                # Create a zip file of the log files and any other files meeting the threshold
+                log_files = [f for f in os.listdir() if f.endswith('.log') and os.stat(f).st_ctime < threshold_time.timestamp()]
+                files_to_archive = log_files # add other files to archive here
+                archive_filename = f'{ARCHIVE_FOLDER}/archive_{datetime.now().strftime("%H-%M_%Y-%m-%d")}.zip'
+                with zipfile.ZipFile(archive_filename, 'w', compression=zipfile.ZIP_DEFLATED) as zip:
+                    for file in files_to_archive:
+                        zip.write(file)
+
+                # Remove the original log files and any other files that were archived
+                for file in files_to_archive:
+                    os.remove(file)
+
+                # Update the creation time of the archive folder
+                os.utime(ARCHIVE_FOLDER, None)
+
+        # Return the connection with the added executemany functionality and first run flag
+        return connection, connection.cursor().execute if not hasattr(connection.cursor(), 'executemany') else connection.cursor().executemany, first_run
     except Exception as e:
         # Log error
         logging.error(f"An error occurred while connecting to the database: {e}")
-        logging.error(traceback.format_exc())
-        raise e
-
-@lru_cache(maxsize=1000)
-def get_sentiment_results():
-    """
-    This function retrieves sentiment analysis results from the database.
-    The results are cached using lru_cache to optimize performance.
-    """
-    try:
-        # Connect to the database
-        conn = connect_to_db()
-        cursor = conn.cursor()
-        # Query the database for the sentiment results
-        query = "SELECT * FROM sentiment_analysis_results"
-        cursor.execute(query)
-        sentiment_results = cursor.fetchall()
-        # Convert the results to a Pandas DataFrame
-        df = pd.DataFrame(sentiment_results, columns=['username', 'text', 'sentiment score'])
-        # Close the database connection
-        cursor.close()
-        conn.close()
-        return df
-    except Exception as e:
-        # Log the error message and stack trace in case of any exceptions
-        logging.error(f"An error occurred while fetching sentiment results: {e}")
-        logging.error(traceback.format_exc())
-        raise e
-
-@lru_cache(maxsize=1000)
-def get_keywords():
-    """
-    This function retrieves keywords from the database.
-    The results are cached using lru_cache to optimize performance.
-    """
-    try:
-        # Connect to the database
-        conn = connect_to_db()
-        cursor = conn.cursor()
-        # Query the database for the keywords
-        query = "SELECT keywords FROM keyword_list"
-        cursor.execute(query)
-        keywords = cursor.fetchall()
-        # Convert the keywords to a list
-        keywords_list = [k[0] for k in keywords]
-        # Close the database connection
-        cursor.close()
-        conn.close()
-        return keywords_list
-    except Exception as e:
-        # Log the error message and stack trace in case of any exceptions
-        logging.error(f"An error occurred while fetching keywords: {e}")
         logging.error(traceback.format_exc())
         raise e
