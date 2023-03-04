48814fc76583fe997243325507c5bbe669d68e7a
Daemon <109057945+Daethyra@users.noreply.github.com>
2023-03-03 21:36:53 -0800

    Delete menu.py

--- menu.py
+++
@@ -1,9 +0,0 @@
-def display_menu():
-    print("1. Scrape tweets and update sentiment analysis results")
-    print("2. View current sentiment analysis results")
-    print("3. Update keyword list")
-    print("4. Quit")
-
-
-def get_choice():
-    return input("Enter your choice: ")
