0454291c122063e18ee29c29e837b14cb2eefe74
Daemon <109057945+Daethyra@users.noreply.github.com>
2023-03-17 14:00:42 -0700

    Update sentiment_analyzer.py

--- sentiment_analyzer.py
+++ sentiment_analyzer.py
@@ -1,17 +1,17 @@
-import re
 import openai
+import re
 
 def preprocess_text(text):
-    text = re.sub(r'http\S+|www\S+|https\S+', '', text, flags=re.MULTILINE) # Remove URLs
-    text = re.sub(r'\@\w+|\#','', text) # Remove @ and #
-    text = re.sub(r'[^a-zA-Z0-9\s]', '', text) # Remove special characters
-    text = text.lower() # Convert to lowercase
-    text = ' '.join([word for word in text.split() if word not in stop_words]) # Remove stopwords
-    return text
+    text = re.sub(r'http\S+', '', text)
+    text = re.sub(r'@\w+', '', text)
+    text = re.sub(r'#\w+', '', text)
+    text = re.sub(r'\n', ' ', text)
+    return text.strip()
 
 def get_sentiment(api_key, text):
     openai.api_key = api_key
-    prompt = f"Sentiment analysis for: {text}. Is the sentiment positive, negative, or neutral?"
-    response = openai.Completion.create(engine="gpt-3.5-turbo", prompt=prompt, max_tokens=1, n=1, temperature=0)
-    sentiment = response.choices[0].text.strip()
+    prompt = f"Please analyze the sentiment of the following text: {text}"
+    response = openai.Completion.create(engine="gpt-3.5-turbo", prompt=prompt, max_tokens=50, n=1, stop=None, temperature=0)
+
+    sentiment = response.choices[0].text.strip().lower()
     return sentiment
