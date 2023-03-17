4ce01890f2b391055dad315304b2ea78c79b8e65
Daemon <109057945+Daethyra@users.noreply.github.com>
2023-03-17 14:00:27 -0700

    Update news_twitter_scraper.py

--- news_twitter_scraper.py
+++ news_twitter_scraper.py
@@ -1,18 +1,21 @@
 import requests
 import tweepy
+from newsapi import NewsApiClient
 
-def get_news_headlines(api_key, sources):
-    url = f"https://newsapi.org/v2/top-headlines?sources={','.join(sources)}&apiKey={api_key}"
-    response = requests.get(url)
-    headlines = [article["title"] for article in response.json()["articles"]]
+def get_news_headlines(api_key, num_sources):
+    newsapi = NewsApiClient(api_key)
+    top_headlines = newsapi.get_top_headlines(language='en', page_size=num_sources)
+    headlines = [article['title'] for article in top_headlines['articles']]
     return headlines
 
-def get_tweets(api_key, api_secret_key, access_token, access_token_secret, threshold):
+def get_tweets(api_key, api_secret_key, access_token, access_token_secret):
     auth = tweepy.OAuthHandler(api_key, api_secret_key)
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
 
     tweets = []
-    for tweet in tweepy.Cursor(api.search_tweets, q="*", lang="en", result_type="recent").items(threshold):
+    usernames = []
+    for tweet in tweepy.Cursor(api.search_tweets, q="*", lang="en", result_type="recent").items():
         tweets.append(tweet.text)
-    return tweets
+        usernames.append(tweet.user.screen_name)
+    return tweets, usernames
