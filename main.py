d86bce4ec1f49399b020ffc183c59cca4ff6af48
Daemon <109057945+Daethyra@users.noreply.github.com>
2023-03-17 13:56:30 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,78 +1,35 @@
-import logging
-from twtapiConn import authenticate_twitter_api
-from sentiment_analyzer import analyze_tweet
-import db_connector
+import os
+from dotenv import load_dotenv
+from news_twitter_scraper import get_news_headlines, get_tweets
+from sentiment_analyzer import preprocess_text, get_sentiment
+from db_manager import init_db, save_sentiment_results, print_db_results
 
+def predict_violence_intentions(sentiments):
+    # Implement your logic to predict the intention of violence based on sentiment analysis results
+    # For this example, we'll assume that any negative sentiment has the potential for violence
+    return ["Violent" if sentiment == "negative" else "Non-violent" for sentiment in sentiments]
 
-def main():
-    # Configure logging
-    logging.basicConfig(filename=db_connector.get_log_filename(), level=logging.INFO,
-                        format='%(asctime)s:%(levelname)s:%(message)s')
+load_dotenv()
 
-    # Check if the log file is older than 7 days
-    db_connector.archive_old_logs()
+NEWSAPI_API_KEY = os.getenv("NEWSAPI_API_KEY")
+TWITTER_API_KEY = os.getenv("TWITTER_API_KEY")
+TWITTER_API_SECRET_KEY = os.getenv("TWITTER_API_SECRET_KEY")
+TWITTER_ACCESS_TOKEN = os.getenv("TWITTER_ACCESS_TOKEN")
+TWITTER_ACCESS_TOKEN_SECRET = os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+user_defined_threshold = int(os.getenv("USER_DEFINED_THRESHOLD"))
 
-    while True:
-        print("1. Scrape tweets and update sentiment analysis results")
-        print("2. View current sentiment analysis results")
-        print("3. Update keyword list")
-        print("4. Quit")
+conn = init_db()
+headlines = get_news_headlines(NEWSAPI_API_KEY, 100_sources)
+tweets = get_tweets(TWITTER_API_KEY, TWITTER_API_SECRET_KEY, TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET, user_defined_threshold)
 
-        choice = input("Enter your choice: ")
+texts = headlines + tweets
+preprocessed_texts = [preprocess_text(text) for text in texts]
+sentiments = [get_sentiment(OPENAI_API_KEY, text) for text in preprocessed_texts]
+violence_intentions = predict_violence_intentions(sentiments)
 
-        if choice in ('1', '2', '3'):
-            try:
-                if choice == '1':
-                    # Get the keywords from the database
-                    keywords = db_connector.get_keywords()
+results = list(zip(texts, sentiments, violence_intentions))
+save_sentiment_results(conn, results)
 
-                    # Connect to the Twitter API
-                    api = authenticate_twitter_api().connect_to_twitter_api()
-
-                    # Scrape tweets using the keywords
-                    tweets = []
-                    for keyword in keywords:
-                        cursor = api.search(q=keyword, lang="en", tweet_mode="extended")
-                        for tweet in cursor:
-                            tweets.append(tweet)
-
-                    # Analyze the sentiment of the tweets
-                    sentiment_results = []
-                    for tweet in tweets:
-                        sentiment = analyze_tweet(tweet)
-                        sentiment_results.append((tweet.user.screen_name, tweet.full_text, sentiment))
-
-                    # Update the sentiment analysis results in the database
-                    db_connector.update_sentiment_results(sentiment_results)
-                    logging.info("Sentiment analysis results updated successfully.")
-                elif choice == '2':
-                    # Get the current sentiment analysis results from the database
-                    sentiment_results = db_connector.get_sentiment_results()
-
-                    # Print the results
-                    print(sentiment_results)
-                    logging.info("Sentiment analysis results retrieved successfully.")
-                elif choice == '3':
-                    # Get the current keywords from the database
-                    keywords = db_connector.get_keywords()
-
-                    # Ask the user for new keywords
-                    new_keywords_str = input("Enter new keywords (comma-separated): ")
-
-                    # Add the new keywords to the database
-                    new_keywords = [k.strip() for k in new_keywords_str.split(",")]
-                    keywords += new_keywords
-                    db_connector.update_keywords(list(set(keywords)))
-
-                    logging.info("Keywords updated successfully.")
-            except Exception as e:
-                logging.error("Error: {}".format(e), exc_info=True)
-                db_connector.log_exception(e)
-        elif choice == '4':
-            break
-        else:
-            print("Invalid choice. Please try again.")
-
-
-if __name__ == '__main__':
-    main()
+print_db_results(conn)
+conn.close()
