55f7a19b817ccf4e422811b9e201bd74ce6a27e5
Daemon <109057945+Daethyra@users.noreply.github.com>
2023-04-07 10:59:37 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,85 +1,77 @@
-import os
-import threading
-from datetime import datetime, timedelta
-from dotenv import load_dotenv
-from queue import Queue
-from news_twitter_scraper import get_news_headlines, stream_tweets
-from sentiment_analyzer import preprocess_text, get_sentiment
-from db_manager import (init_db, save_sentiment_results, print_db_results, 
-                        flag_dangerous_users, compress_old_results)
-from spinner import start_animation
+import requests
+import json
+from scholarly import scholarly
+from multiprocessing import Pool
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from operator import itemgetter
 
-def predict_violence_intentions(sentiments):
-    return ["Violent" if sentiment == "negative" else "Non-violent" for sentiment in sentiments]
 
-def analyze_text_worker(q, api_key, stop_event):
-    while not stop_event.is_set():
-        try:
-            text_data = q.get(timeout=1)
-            preprocessed_text = preprocess_text(text_data)
-            sentiment_result = get_sentiment(api_key, preprocessed_text)
-            save_sentiment_results(conn, text_data, sentiment_result)
-            q.task_done()
+def search_database(args):
+    db, search_keywords = args
+    return globals()[db["search_func"]](search_keywords)
 
-            # Check if the user is flagged
-            user_id = sentiment_result["user_id"]
-            if user_id:
-                flagged_user = flag_dangerous_users(conn, user_id, USER_DEFINED_THRESHOLD)
-                if flagged_user:
-                    print(f"Alert! Dangerous user: {flagged_user}")
 
-        except Empty:
-            pass
+def main():
+    databases = [
+        {
+            "name": "Google Scholar",
+            "search_func": "search_google_scholar"
+        },
+        {
+            "name": "PubMed",
+            "search_func": "search_pubmed"
+        },
+        {
+            "name": "arXiv",
+            "search_func": "search_arxiv"
+        },
+        {
+            "name": "IEEE Xplore",
+            "search_func": "search_ieee_xplore"
+        }
+    ]
 
-load_dotenv()
+    search_keywords = input("Enter search keywords: ")
+    num_results = int(input("Enter the number of results to display: "))
 
-NEWSAPI_API_KEY = os.getenv("NEWSAPI_API_KEY")
-TWITTER_API_KEY = os.getenv("TWITTER_API_KEY")
-TWITTER_API_SECRET_KEY = os.getenv("TWITTER_API_SECRET_KEY")
-TWITTER_ACCESS_TOKEN = os.getenv("TWITTER_ACCESS_TOKEN")
-TWITTER_ACCESS_TOKEN_SECRET = os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-USER_DEFINED_THRESHOLD = int(os.getenv("USER_DEFINED_THRESHOLD"))
+    def search_google_scholar(query):
+        search_query = scholarly.search_pubs(query)
+        return [next(search_query).fill() for _ in range(num_results)]
 
-conn = init_db()
+    """def search_pubmed(query):
+        API_KEY = 'your_pubmed_api_key_here'
+        BASE_URL = 'https://api.ncbi.nlm.nih.gov/lit/abstract/v1'
+        response = requests.get(f'{BASE_URL}?db=pubmed&term={query}&retmax={num_results}&api_key={API_KEY}')
+        return response.json()['result']['docs']"""
 
-headlines = get_news_headlines(NEWSAPI_API_KEY)
-text_queue = Queue()
-stop_event = threading.Event()
+    def search_arxiv(query):
+        BASE_URL = 'http://export.arxiv.org/api/query'
+        response = requests.get(f'{BASE_URL}?search_query=all:{query}&start=0&max_results={num_results}')
+        return response.json()['entries']
 
-for i in range(5):  # Number of threads
-    t = threading.Thread(target=analyze_text_worker, args=(text_queue, OPENAI_API_KEY, stop_event))
-    t.daemon = True
-    t.start()
+    """def search_ieee_xplore(query):
+        API_KEY = 'your_ieee_api_key_here'
+        BASE_URL = 'https://ieeexploreapi.ieee.org/api/v1/search/articles'
+        headers = {'X-IEEE-Api-Key': API_KEY}
+        params = {'querytext': query, 'max_records': num_results}
+        response = requests.get(BASE_URL, headers=headers, params=params)
+        return response.json()['articles']"""
 
-for headline in headlines:
-    text_queue.put(headline)
+    with Pool(len(databases)) as pool:
+        articles_lists = pool.map(search_database, [(db, search_keywords) for db in databases])
 
-stream_thread = threading.Thread(target=stream_tweets, args=(TWITTER_API_KEY, TWITTER_API_SECRET_KEY, TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET, text_queue))
-stream_thread.daemon = True
-stream_thread.start()
+    articles = [article for articles_list in articles_lists for article in articles_list]
 
-spinner_thread = threading.Thread(target=start_animation)  # Start the spinner animation
-spinner_thread.daemon = True
-spinner_thread.start()
+    articles_with_scores = [{"article": article, "score": relevance_score(article)} for article in articles]
 
-try:
-    while True:
-        command = input("Enter 'print' to display the table or 'stop' to stop the scraping: ").strip().lower()
-        if command == "print":
-            stop_event.set()  # Stop the spinner temporarily
-            spinner_thread.join()
-            print_db_results(conn)
-            flagged_users = flag_dangerous_users(conn, threshold=USER_DEFINED_THRESHOLD)
-            print("Alert! Dangerous users:", flagged_users)
-            spinner_thread = threading.Thread(target=start_animation)  # Restart the spinner
-            spinner_thread.daemon = True
-            spinner_thread.start()
-            stop_event.clear()
-        elif command == "stop":
-            stop_event.set()
-            break
-finally:
-    text_queue.join()
-    compress_old_results(conn, timedelta(days=1))
-    conn.close()
+    sorted_articles = sorted(articles_with_scores, key=itemgetter("score"), reverse=True)
+
+    print(f"\nTop {num_results} results:")
+    for i, item in enumerate(sorted_articles[:num_results], start=1):
+        print(f"{i}. {item['article']['title']} - {item['article']['author_info']}")
+        print(f"Abstract: {item['article']['abstract']}\n")
+
+if __name__ == '__main__':
+    main()
