ec8a302b7ef78585ce5dfd5207d926667292ac02
Daemon <109057945+Daethyra@users.noreply.github.com>
2023-04-07 16:24:38 -0700

    Add files via upload

--- message_queue.py
+++ message_queue.py
@@ -1,13 +1,20 @@
 import pika
 import json
+import logging
 from config import RABBITMQ_URL
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
 def send_message(queue, message):
-    connection = pika.BlockingConnection(pika.URLParameters(RABBITMQ_URL))
-    channel = connection.channel()
-    channel.queue_declare(queue=queue)
-    channel.basic_publish(exchange='', routing_key=queue, body=json.dumps(message))
-    connection.close()
+    try:
+        connection = pika.BlockingConnection(pika.URLParameters(RABBITMQ_URL))
+        channel = connection.channel()
+        channel.queue_declare(queue=queue)
+        channel.basic_publish(exchange='', routing_key=queue, body=json.dumps(message))
+        connection.close()
+    except Exception as e:
+        logger.error(f"Error in send_message: {e}")
 
 def receive_messages(queue, callback):
     connection = pika.BlockingConnection(pika.URLParameters(RABBITMQ_URL))
