7f06efe46dc7b5bddd2d310341d9de924ce99f88
Daemon <109057945+Daethyra@users.noreply.github.com>
2023-08-03 00:09:29 -0700

    modified:   project/modules/CyberSentinel/preprocessor.py 	modified:   project/modules/CyberSentinel/retry_w_backoff.py

--- project/modules/CyberSentinel/preprocessor.py
+++ project/modules/CyberSentinel/preprocessor.py
@@ -7,10 +7,7 @@ import string
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.cluster import KMeans
 from tqdm import tqdm
-import joblib
 from PyPDF2 import PdfReader
 
 class FileProcessor:
@@ -22,7 +19,65 @@ class FileProcessor:
         self.lemmatizer = WordNetLemmatizer()
         logging.info('FileProcessor initialized.')
 
-    def process_files(self):
+    def clean_text(self, text):
+        text = text.lower()
+        text = re.sub(r"http\\S+|www\\S+|https\\S+", '', text, flags=re.MULTILINE)
+        text = re.sub(r'\\@\\w+|\\#','', text)
+        text = text.translate(str.maketrans('', '', string.punctuation))
+        text_tokens = word_tokenize(text)
+        text = [word for word in text_tokens if word not in stopwords.words('english')]
+        text = [self.lemmatizer.lemmatize(word) for word in text]
+        text = [word for word in text if len(word) > 2]
+        return ' '.join(text)
+
+    def process_pdf(self, file_path):
+        with open(file_path, 'rb') as file:
+            pdf_reader = PdfReader(file)
+            text = ''
+            for page in pdf_reader.pages:
+                text += page.extract_text()
+        return self.clean_text(text)
+
+    def process_csv(self, file_path, delimiter=','):
+        df = pd.read_csv(file_path, delimiter=delimiter)
+        text = ' '.join(df.apply(lambda row: ' '.join(row.astype(str)), axis=1))
+        return self.clean_text(text)
+
+    def process_tsv(self, file_path):
+        return self.process_csv(file_path, delimiter='\\t')
+
+    def process_txt(self, file_path):
+        with open(file_path, 'r', encoding='utf-8') as file:
+            text = file.read()
+        return self.clean_text(text)
+
+    def process_json(self, file_path):
+        with open(file_path, 'r', encoding='utf-8') as file:
+            json_data = json.load(file)
+            text = ' '.join(str(value) for value in json_data.values())
+        return self.clean_text(text)
+
+    def process_file(self, file_path):
+        assert isinstance(file_path, str), "file_path must be a string"
+        try:
+            if file_path.endswith('.pdf'):
+                return self.process_pdf(file_path)
+            elif file_path.endswith('.csv'):
+                return self.process_csv(file_path)
+            elif file_path.endswith('.tsv'):
+                return self.process_tsv(file_path)
+            elif file_path.endswith('.txt'):
+                return self.process_txt(file_path)
+            elif file_path.endswith('.json'):
+                return self.process_json(file_path)
+            else:
+                logging.warning(f'Unsupported file type: {file_path}')
+                return None
+        except Exception as e:
+            logging.error(f'Error processing {file_path}: {e}')
+            return None
+
+    def process_files(self, output_csv_filename):
         assert os.path.exists(self.data_directory), f'The directory "{self.data_directory}" does not exist.'
         assert os.listdir(self.data_directory), f'The directory "{self.data_directory}" is empty.'
         data = []
@@ -45,136 +100,24 @@ class FileProcessor:
                     logging.error(f'Error processing {file_path}: {e}')
                     continue
         logging.info('File processing completed.')
-        return data
-
-    def process_file(self, file_path):
-        assert isinstance(file_path, str), "file_path must be a string"
-        try:
-            if file_path.endswith('.pdf'):
-                return self.process_pdf(file_path)
-            elif file_path.endswith('.csv'):
-                return self.process_csv(file_path, delimiter=',')
-            elif file_path.endswith('.tsv'):
-                return self.process_csv(file_path, delimiter='\t')
-            elif file_path.endswith('.txt'):
-                return self.process_txt(file_path)
-            elif file_path.endswith('.json'):
-                return self.process_json(file_path)
-            elif file_path.endswith('.xls'):
-                return self.process_xls(file_path)
-            else:
-                logging.warning(f'Unsupported file format: {file_path}')
-                return ''
-        except Exception as e:
-            logging.error(f'Error processing {file_path}: {e}')
-            return ''
-
-    def process_pdf(self, file_path):
-        logging.debug(f'Reading PDF file: {file_path}')
-        with open(file_path, 'rb') as file:
-            reader = PdfReader(file)
-            text = '\n'.join(page.extract_text() for page in reader.pages)
-        return self.clean_text(text)
-
-    def process_csv(self, file_path, delimiter):
-        logging.debug(f'Reading CSV file: {file_path}')
-        df = pd.read_csv(file_path, encoding='utf-8', delimiter=delimiter, low_memory=False)
-        text = ' '.join(df.astype(str).values.flatten())
-        return self.clean_text(text)
-
-    def process_txt(self, file_path):
-        logging.debug(f'Reading TXT file: {file_path}')
-        with open(file_path, 'r', encoding='utf-8') as file:
-            text = file.read()
-        return self.clean_text(text)
-
-    def process_json(self, file_path):
-        logging.debug(f'Reading JSON file: {file_path}')
-        with open(file_path, 'r', encoding='utf-8') as file:
-            texts = []
-            for line in file:
-                try:
-                    data = json.loads(line)
-                    text = json.dumps(data)
-                    texts.append(text)
-                except json.JSONDecodeError as e:
-                    logging.error(f'Error decoding JSON line in {file_path}: {e}')
-            return self.clean_text(' '.join(texts))
-
-    def process_xls(self, file_path):
-        logging.debug(f'Reading XLS file: {file_path}')
-        df = pd.read_excel(file_path)
-        text = ' '.join(df.astype(str).values.flatten())
-        return self.clean_text(text)
+        # Saving the processed data to CSV
+        self.save_to_csv(data, output_csv_filename)
 
     def save_to_csv(self, data, filename):
-        df = pd.DataFrame(data, columns=['text', 'file_path'])
+        df = pd.DataFrame(data)
         df.to_csv(filename, index=False)
-
-    def load_from_csv(self, filename):
-        return pd.read_csv(filename)
-
-    def process_and_save(self, filename):
-        texts = self.process_files()
-        # Check for duplicates
-        df = pd.DataFrame(texts)
-        duplicates = df[df.duplicated()]
-        if duplicates.shape[0] > 0:
-            logging.warning(f"Found {duplicates.shape[0]} duplicate records.")
-            df = df.drop_duplicates()
-        # Check for null values
-        if df.isnull().sum().sum() > 0:
-            logging.warning("Found null values, filling with empty strings.")
-            df = df.fillna("")
-        self.save_to_csv(df, filename)
-
-    def clean_text(self, text):
-        # Lowercase the text
-        text = text.lower()
-        # Remove URLs
-        text = re.sub(r"http\S+|www\S+|https\S+", '', text, flags=re.MULTILINE)
-        # Remove user @ references and '#' from text
-        text = re.sub(r'\@\w+|\#','', text)
-        # Remove punctuations
-        text = text.translate(str.maketrans('', '', string.punctuation))
-        # Remove stopwords
-        text_tokens = word_tokenize(text)
-        text = [word for word in text_tokens if word not in stopwords.words('english')]
-        # Lemmatize words
-        text = [self.lemmatizer.lemmatize(word) for word in text]
-        # Remove words with 2 or fewer letters
-        text = [word for word in text if len(word) > 2]
-        return text
-
-
-class UnsupervisedModelTrainer:
-    def __init__(self, data_directory):
-        self.file_processor = FileProcessor(data_directory)
-        self.data = self.file_processor.load_from_csv("processed_data.csv")["text"]
-
-    def train_model(self):
-        vectorizer = TfidfVectorizer()
-        X = vectorizer.fit_transform(self.data)  # Use only the 'text' column for vectorization
-        model = KMeans(n_clusters=5, n_init=10)
-        model.fit(X)
-        return model
-
-    def save_model(self, model, filename):
-        joblib.dump(model, filename)
-
-    def load_model(self, filename):
-        return joblib.load(filename)
-
-    def train_and_save(self, filename):
-        model = self.train_model()
-        self.save_model(model, filename)
-
+        logging.info(f'Data saved to CSV file: {filename}')
 
 if __name__ == "__main__":
-    script_directory = os.path.dirname(os.path.abspath(__file__))
-    data_directory = os.path.join(script_directory, "../../data")
-    file_processor = FileProcessor(data_directory)
-    file_processor.process_and_save("processed_data.csv")
+    import sys
+    
+    if len(sys.argv) != 3:
+        print("Usage: python preprocessor.py <data_directory> <output_csv_filename>")
+        sys.exit(1)
+    
+    data_directory = sys.argv[1]
+    output_csv_filename = sys.argv[2]
 
-    model_trainer = UnsupervisedModelTrainer(data_directory)
-    model_trainer.train_and_save("unsupervised_model.joblib")
+    file_processor = FileProcessor(data_directory)
+    file_processor.process_files(output_csv_filename)
+    print(f"Preprocessing completed. Processed data saved to {output_csv_filename}.")
--- project/modules/CyberSentinel/retry_w_backoff.py
+++ project/modules/CyberSentinel/retry_w_backoff.py
@@ -1,36 +1,45 @@
-"""
-Retry w/ exponential backoff | Tested and functional
-"""
-
 import asyncio
 import logging
-from typing import Callable, Awaitable
+import random
+from typing import Any, Callable, Awaitable, Tuple, Type
 
 async def retry_with_backoff(
-    func: Callable[..., Awaitable],
+    func: Callable[..., Awaitable[Any]],
+    *args: Any,
     max_retries: int = 3,
-    backoff_values: list[float] = [4],
-) -> None:
+    initial_delay: float = 1,
+    backoff_factor: float = 2,
+    retry_exceptions: Tuple[Type[Exception]] = (Exception,),
+    **kwargs: Any,
+) -> Any:
     """
     Retry the given async function with exponential backoff.
 
     Args:
-        func (Callable[..., Awaitable]): The async function to retry.
-        max_retries (int): Maximum number of retries (default: 3).
-        backoff_values (list[float]): List of backoff values in seconds (default: [4]).
+        func (Callable[..., Awaitable[Any]]): The async function to retry.
+        *args (Any): Optional positional arguments to pass to the function.
+        max_retries (int): Optional. Maximum number of retries (default: 3).
+        initial_delay (float): Optional. Initial delay in seconds (default: 1).
+        backoff_factor (float): Optional. Factor to multiply the delay by after each retry (default: 2).
+        retry_exceptions (Tuple[Type[Exception]]): Optional. Exceptions to catch for retries (default: all exceptions).
+        **kwargs (Any): Optional keyword arguments to pass to the function.
 
     Returns:
-        None
+        The result of the successful function call, or raises the last exception if all retries fail.
     """
+    delay = initial_delay
+    last_exception = None
     for retry in range(max_retries):
         try:
-            await func()
-            break  # Success, no need to retry
-        except Exception as e:
+            return await func(*args, **kwargs)  # Return the result on success
+        except retry_exceptions as e:
+            last_exception = e
             logging.error(f"An error occurred: {e}")
             if retry == max_retries - 1:
                 logging.error(f"Max retries exceeded.")
-                break  # Max retries reached, give up
-            delay = backoff_values[min(retry, len(backoff_values) - 1)]
-            logging.warning(f"Retrying in {delay} seconds... ({retry + 1}/{max_retries})")
-            await asyncio.sleep(delay)
+                raise last_exception  # Raise the last exception if max retries reached
+            jitter = random.uniform(0.5, 1.5)  # Random factor between 0.5 and 1.5
+            actual_delay = delay * jitter
+            logging.warning(f"Retrying in {actual_delay} seconds... ({retry + 1}/{max_retries})")
+            await asyncio.sleep(actual_delay)
+            delay *= backoff_factor  # Increase the delay for the next retry
